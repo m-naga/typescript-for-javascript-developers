bd7a0e32074c63157e0001d24ab2816fe0068c74
m-nagasawa <m-nagasawa@sakura.ad.jp>
2021-10-17 14:21:47 +0900

    add array.ts

---
+++ src/array.ts
@@ -0,0 +1,15 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [4, 5, 6]
+let strings2: Array<string> = ['Tokyo', 'Osaka', 'Kyoto'];
+
+let strings: string[] = ["TypeScript", 'Javascript', 'CoffeeScript'];
+
+let ninjigenHariretsu: number[][] = [
+  [40, 100],
+  [140,480]
+];
+
+let hairetsu: (boolean | number | String)[] = [1, false, 'Japan'];
